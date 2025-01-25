95913a12af3857d14f29ef8e33bf7f362bc44e79
Frederik Holfeld <freddy@frederik-holfeld.net>
2025-01-25 14:22:05 +0100

    Initial commit
    This project is currently used by my BRsP Blender clustering application for handling frame rendering.

---
+++ br.py
@@ -0,0 +1,37 @@
+import bpy
+import json
+from pathlib import Path
+import socket
+import sys
+
+args = sys.argv[sys.argv.index("--") + 1:]
+connection = socket.create_connection(("localhost", args[0]))
+
+size = bytearray(1)
+connection.recv_into(size)
+
+header = bytearray(int.from_bytes(size))
+connection.recv_into(header)
+
+header = json.loads(header)
+match header["type"]:
+    case "render":
+        bpy.ops.wm.open_mainfile(filepath=str(
+            Path(header["blend"]).absolute()
+        ))
+
+        frame = header["frame"]
+        bpy.context.scene.frame_current = frame
+
+        path = str(Path(header["output"]) / f"{frame:04}")
+        bpy.context.scene.render.filepath = path
+        bpy.context.scene.render.use_file_extension = True
+
+        bpy.ops.render.render(write_still=True)
+
+        response = json.dumps({
+            "type": "okay",
+            "image": path + bpy.context.scene.render.file_extension
+        }).encode()
+
+        connection.sendall(len(response).to_bytes() + response)
