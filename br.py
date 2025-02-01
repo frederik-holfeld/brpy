5f2aaa7e4ed3743c42eab531e840bee9ae5929e5
Frederik Holfeld <freddy@frederik-holfeld.net>
2025-02-02 00:38:52 +0100

    Make process persist for multiple consecutive requests

--- br.py
+++ br.py
@@ -7,31 +7,36 @@ import sys
 args = sys.argv[sys.argv.index("--") + 1:]
 connection = socket.create_connection(("localhost", args[0]))
 
-size = bytearray(1)
-connection.recv_into(size)
+old_blend = None
+while True:
+    size = bytearray(1)
+    connection.recv_into(size)
 
-header = bytearray(int.from_bytes(size))
-connection.recv_into(header)
+    header = bytearray(int.from_bytes(size))
+    connection.recv_into(header)
 
-header = json.loads(header)
-match header["type"]:
-    case "render":
-        bpy.ops.wm.open_mainfile(filepath=str(
-            Path(header["blend"]).absolute()
-        ))
+    header = json.loads(header)
+    match header["type"]:
+        case "render":
+            if header["blend"] != old_blend:
+                bpy.ops.wm.open_mainfile(filepath=str(
+                    Path(header["blend"]).absolute()
+                ))
 
-        frame = header["frame"]
-        bpy.context.scene.frame_current = frame
+                old_blend = header["blend"]
 
-        path = str(Path(header["output"]) / f"{frame:04}")
-        bpy.context.scene.render.filepath = path
-        bpy.context.scene.render.use_file_extension = True
+            frame = header["frame"]
+            bpy.context.scene.frame_current = frame
 
-        bpy.ops.render.render(write_still=True)
+            path = str(Path(header["output"]) / f"{frame:04}")
+            bpy.context.scene.render.filepath = path
+            bpy.context.scene.render.use_file_extension = True
 
-        response = json.dumps({
-            "type": "okay",
-            "image": path + bpy.context.scene.render.file_extension
-        }).encode()
+            bpy.ops.render.render(write_still=True)
 
-        connection.sendall(len(response).to_bytes() + response)
+            response = json.dumps({
+                "type": "okay",
+                "image": path + bpy.context.scene.render.file_extension
+            }).encode()
+
+            connection.sendall(len(response).to_bytes() + response)
